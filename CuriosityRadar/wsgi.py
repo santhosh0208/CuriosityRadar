11c450f10970bb0d1277f26beed84fc174cff920
santhosh0208 <santhosh.k.02abc@gmail.com>
2020-07-29 09:36:41 +0530

    added files and changes required for deployment on heroku

--- CuriosityRadar/wsgi.py
+++ CuriosityRadar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for QuestionIt project.
+WSGI config for CuriosityRadar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuestionIt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CuriosityRadar.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuestionIt.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CuriosityRadar.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
